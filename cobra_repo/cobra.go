81474d5074aec8c06db6b858d0f6fe5c1eddada9
lihang.wyq <lihang.wyq@bytedance.com>
2022-03-08 17:55:40 +0800

    log

--- cobra_repo/cobra.go
+++ cobra_repo/cobra.go
@@ -44,7 +44,7 @@ var logCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		output, err := ExecuteCommand("git", "log", args...)
 		if err != nil {
-			Error(cmd, args, err)
+			// Error(cmd, args, err)
 		}
 
 		fmt.Fprint(os.Stdout, output)
